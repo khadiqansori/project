d3f3181a987cf5020735b585af68b01e7f7ba29e
rahmadantijp17 <44959173+rahmadantijp17@users.noreply.github.com>
2019-11-19 10:16:18 +0700

    Coba upload file contact
    test

---
+++ contact.php
@@ -0,0 +1,18 @@
+    <div class="container mt-1">
+        <div class="row">
+            <div class="col">
+                <h4>Contact us</h4>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <a href="mailto:internal.communications@sampoerna.com" style="color: #4d3372"><b style="color: #007bff">Click here</b> for any inquires</a>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-10">
+                <img src="assets/img/logo2.png" width="130" class="img-fluid mb-3">
+                <p align="justify" style="color: #4d3372">Lentera was developed by people and culture especially by Internal Communication to deliver news for a all employee in Indonesia. For now Lentera has four channel : Sampoerna News, Tv Sampoerna, Local One Place, and Screensaver.</p>
+            </div>
+        </div>
+    </div>
